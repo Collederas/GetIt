acebbc0a594649d1e8bca92ef406bc94c62ca51c
Fabrizio Scandola <fabrizio.scandola@elements.nl>
2021-01-30 21:29:55 +0100

    f

---
+++ Assets/Scripts/Level/WallTile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "WallTile", menuName = "Tiles/Wall")]
+public class WallTile : TileBase
+{
+    public Sprite tileSprite;
+    public Tile.ColliderType colliderType;
+    public GameObject tilePrefab;
+    
+    
+    public override void GetTileData(Vector3Int position, ITilemap tileMap, ref TileData tileData){
+        tileData.sprite = tileSprite;
+        tileData.colliderType = colliderType;
+        tileData.flags = TileFlags.LockTransform;
+        tileData.transform = Matrix4x4.identity;
+        tileData.gameObject = tilePrefab;
+    }
+}
