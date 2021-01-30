6a0146f807c1c5d5777affb8309e332d8c981736
Fabrizio Scandola <fabrizio.scandola@elements.nl>
2021-01-30 12:26:38 +0100

    Initial Commit

---
+++ Assets/Scripts/Controllers/Animate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MovingObjectController))]
+[RequireComponent(typeof(Animator))]
+public class Animate : MonoBehaviour
+{
+    private Animator animator;
+    private MovingObjectController movingObject;
+    private Vector2 velocityNormalized;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        movingObject = GetComponent<MovingObjectController>();
+    }
+
+    private void Update()
+    {
+        velocityNormalized = movingObject.Velocity.normalized;
+
+        var vertical = Vector2.Dot(velocityNormalized, Vector2.up);
+        var horizontal = Vector2.Dot(velocityNormalized, Vector2.right);
+        var speed = movingObject.Velocity.magnitude / Time.deltaTime;
+
+        if ((horizontal != 0) | (vertical != 0))
+        {
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Vertical", vertical);
+        }
+
+        animator.SetFloat("Speed", speed);
+    }
+}
---
+++ Assets/Scripts/Controllers/MovingObjectController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public abstract class MovingObjectController : MonoBehaviour
+{
+    public float maxSpeed = 2f;
+    public Vector2 Velocity { get; set; }
+
+    protected virtual void Update()
+    {
+        Velocity = Vector2.ClampMagnitude(Velocity, maxSpeed);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        var delta = Velocity * Time.deltaTime;
+        PerformMovement(delta);
+    }
+
+    protected void PerformMovement(Vector2 delta)
+    {
+        // Transform is only a Vector3 so build delta as a 3D vector. 
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+
+    public virtual void Fall(Vector2 fallTargetPosition)
+    {
+    }
+}
