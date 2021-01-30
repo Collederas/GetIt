6e1ba77ead96b235451424a60c3c868cf89d7f35
Fabrizio Scandola <fabrizio.scandola@elements.nl>
2021-01-30 21:51:56 +0100

    Add SpawnGroup functionality

--- Assets/Scripts/Level/LevelBuilder.cs
+++ Assets/Scripts/Level/LevelBuilder.cs
@@ -6,6 +6,7 @@ using Random = UnityEngine.Random;
 
 public class LevelBuilder : MonoBehaviour
 {
+   
     public TileBase wallTile;
     public Vector2Int levelBoundaries;
     public int maxTilesInGroup = 5;
@@ -46,13 +47,44 @@ public class LevelBuilder : MonoBehaviour
         return _positionIteratorRef.y < -(levelBoundaries.y/2);
     }
 
+    private bool IsPositionOutOfBounds(Vector3Int position)
+    {
+        return _positionIteratorRef.y > Mathf.Abs(levelBoundaries.y/2) || _positionIteratorRef.x > Mathf.Abs(levelBoundaries.x/2);
+    }
+
+    private Vector3Int SpawnPosRepresentationToCoord(Vector3Int position, int spawnPosIndicator)
+    {
+        return spawnPosIndicator switch
+        {
+            1 => new Vector3Int(position.x + 1, position.y, position.z),
+            2 => new Vector3Int(position.x, position.y + 1, position.z),
+            3 => new Vector3Int(position.x - 1, position.y, position.z),
+            4 => new Vector3Int(position.x, position.y - 1, position.z),
+            _ => Vector3Int.zero
+        };
+    }
+    
+    private void SpawnTileGroup(Vector3Int position)
+    {
+        var spawnedTiles = 0;
+        while (spawnedTiles < Random.Range(1, maxTilesInGroup))
+        {
+            var spawnPosRepr= Random.Range(1, 4);
+            var spawnPos = SpawnPosRepresentationToCoord(position, spawnPosRepr);
+            
+            if (!IsPositionOutOfBounds(spawnPos))
+                levelTilemap.SetTile(spawnPos, wallTile);
+            
+            spawnedTiles++;
+        }
+    }
+
     private void MoveIteratorRef()
     {
         var moveAmountX = Random.Range(spacing, spacing + 3);
-        Debug.Log("Move by " + moveAmountX);
-
         var newPositionX = _positionIteratorRef.x + moveAmountX;
         var amountBeyondBoundaryX = newPositionX - (levelBoundaries.x / 2);
+        
         if (amountBeyondBoundaryX > 0)
         {
             // We are beyond boundary, so we start over from next line
@@ -79,11 +111,10 @@ public class LevelBuilder : MonoBehaviour
     {
         while (!OutOfLevelBounds())
         {
-            var spawnPoint = GetNewSpawnPoint();
+            var spawnPos = GetNewSpawnPoint();
             if (!OutOfLevelBounds())
             {
-                levelTilemap.SetTile(spawnPoint, wallTile);
-            
+                SpawnTileGroup(spawnPos);
             }
         }
         Debug.Log("Finish at " + _positionIteratorRef);
